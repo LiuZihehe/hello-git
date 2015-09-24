feedca01c6f666a21f30b2bcb9bd8405abe98431
LiuZihe <liuzihe0@gmail.com>
2015-09-24 21:51:37 +0800

    Just for fun.

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **
+dfgsdglargv)
+{sdfoj dsljk ksldj ;skflj 
 	cout << "Hello world!" << endl;
 	return 0;
 }
